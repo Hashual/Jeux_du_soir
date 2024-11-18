fa8f21ab1657d267746222e007c1a17aec898c6f
Hashual <loizeau.dorian05@gmail.com>
2024-11-18 15:15:01 +0100

    add view for back card

---
+++ components/backCard.tsx
@@ -0,0 +1,51 @@
+// components/backCard.tsx
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+interface BackCardProps {
+  isFaceUp: boolean; // Indique si la carte est face visible ou non
+  onPress: () => void; // Fonction de gestion du clic sur la carte
+}
+
+const BackCard: React.FC<BackCardProps> = ({ isFaceUp, onPress }) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.card}>
+      <View style={[styles.cardContent, isFaceUp && styles.faceUp]}>
+        {isFaceUp ? (
+          <Text style={styles.cardText}>Carte affichée</Text> // Affichage du contenu de la carte quand elle est face visible
+        ) : (
+          <Text style={styles.cardText}>CARD</Text> // Affichage du dos de la carte
+        )}
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    width: 100,
+    height: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 10,
+  },
+  cardContent: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#EEE',
+  },
+  faceUp: {
+    backgroundColor: '#FFF',
+    borderColor: '#000',
+  },
+  cardText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default BackCard;
